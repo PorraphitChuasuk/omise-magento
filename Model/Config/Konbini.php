85fdaa1416f969aa4e6089d15efec8c8085d91c3
Jacek <jacek.stanusz@gmail.com>
2019-12-12 10:54:08 +0700

    renamed convenience store Config name to konbini

--- Model/Config/Konbini.php
+++ Model/Config/Konbini.php
@@ -3,10 +3,10 @@ namespace Omise\Payment\Model\Config;
 
 use Omise\Payment\Model\Config\Config;
 
-class Conveniencestore extends Config
+class Konbini extends Config
 {
     /**
      * @var string
      */
-    const CODE = 'omise_offline_conveniencestore';
+    const CODE = 'omise_offline_konbini';
 }
