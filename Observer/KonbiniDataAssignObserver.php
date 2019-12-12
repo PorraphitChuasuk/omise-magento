a5d8c4cc0ae167288fed53c44989c60019f6e152
Jacek <jacek.stanusz@gmail.com>
2019-12-12 10:45:51 +0700

    renamed convenience store data assign observers to match konbini name

--- Observer/KonbiniDataAssignObserver.php
+++ Observer/KonbiniDataAssignObserver.php
@@ -5,7 +5,7 @@ use Magento\Framework\Event\Observer;
 use Magento\Payment\Observer\AbstractDataAssignObserver;
 use Magento\Quote\Api\Data\PaymentInterface;
 
-class ConveniencestoreDataAssignObserver extends AbstractDataAssignObserver
+class KonbiniDataAssignObserver extends AbstractDataAssignObserver
 {
     /**
      * @var string
